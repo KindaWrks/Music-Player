86865e8b37d4c23538391a56ba26e99e56ab3358
KindaWrks <165777334+KindaWrks@users.noreply.github.com>
2024-04-24 18:40:35 -0400

    can now click on songlist display to choose song
    removed music label added songlist func

--- MusicNode.gd
+++ MusicNode.gd
@@ -2,10 +2,8 @@ extends Node2D
 
 @onready var fd_open = $FD_OPENfiles
 
-@onready var music_label = $MusicLabel
 @onready var music_time_label = $MusicTimeLabel
 @onready var music_total_time = $MusicTotalTimeLabel
-
 
 @export var audio_bus_name := "Master" #Makes the var Master audio bus
 @onready var _bus := AudioServer.get_bus_index(audio_bus_name) #Sets bus index to Master
@@ -17,7 +15,6 @@ var musicindex = 0
 func _ready():
 	fd_o_filters()
 	fd_open.visible = true
-	music_label.visible = false
 
 func fd_o_filters():
 	fd_open.current_dir = "/"  #Set current dir as Root
@@ -25,7 +22,6 @@ func fd_o_filters():
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(_delta):
-	music_label.text = str(musicarray).get_file().get_basename() #Gets the current file and removes the .MP3
 	
 	if $AudioStreamPlayer.playing: #Make time count down in int rather then float and show total time
 		music_time_label.text = str(int($AudioStreamPlayer.stream.get_length()) - int($AudioStreamPlayer.get_playback_position()))
@@ -46,14 +42,13 @@ func _on_fd_open_files_selected(paths):
 		snd_file.close() # Close file.
 		$AudioStreamPlayer.stream = stream  # The loaded song in memory
 		$AudioStreamPlayer.play()
-		music_label.visible = true
 
 		# Add the selected song to the queue
 		musicarray.append(path)
 		
 		#Add song list
-		#var file_name_without_extension = path.get_file().get_basename()
-		#$song_list.add_item(file_name_without_extension)
+		var file_name_without_extension = path.get_file().get_basename()
+		$song_list.add_item(file_name_without_extension)
 
 func play_next_song():
 	var next_song_path = musicarray[musicindex] # Get the next song from the queue
@@ -64,9 +59,23 @@ func play_next_song():
 	$AudioStreamPlayer.stream = stream  #The loaded song in memory
 	$AudioStreamPlayer.play()
 
-func _on_buttonshow_pressed():
+func _on_buttonshow_pressed(): #Show Filedialog
 	fd_open.visible = true
 	
 func _on_h_slider_value_changed(value: float):
 	AudioServer.set_bus_volume_db(_bus,linear_to_db(value)) #Control volume as a float
 
+# Called when an item in the song list is selected
+func _on_song_list_item_selected(index):
+	if index < musicarray.size():
+		var selected_song_path = musicarray[index]
+		play_song(selected_song_path)
+
+# Function to play a selected song
+func play_song(song_path):
+	var snd_file = FileAccess.open(song_path, FileAccess.READ)
+	var stream = AudioStreamMP3.new()
+	stream.data = snd_file.get_buffer(snd_file.get_length())
+	snd_file.close()
+	$AudioStreamPlayer.stream = stream
+	$AudioStreamPlayer.play()
