0bfaf84c56fe69d1398abd53cee3975c7768f9f4
KindaWrks <165777334+KindaWrks@users.noreply.github.com>
2024-05-08 07:56:44 -0400

    add simple X for click closing

--- MusicNode.gd
+++ MusicNode.gd
@@ -262,3 +262,8 @@ func _input(event):
 	if event is InputEventMouseButton:
 		if event.button_index == MOUSE_BUTTON_LEFT and not event.pressed:
 			selected = false
+
+
+func _on_area_2dx_input_event(_viewport, _event, _shape_idx):
+	if Input.is_action_just_pressed("left_click"):
+		get_tree().quit()
